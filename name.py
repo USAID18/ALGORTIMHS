eaa8891fb95fec69b1530404379482c1145e127d
USAID198 <imranjamilke@gmail.com>
2024-12-26 20:52:24 +0500

    message

---
+++ name.py
@@ -0,0 +1,20 @@
+def printNm():
+    var1=4
+    var2=8
+    print( var1*var2)
+printNm()
+
+# 2
+
+print(2*4)
+
+# 3
+
+
+var1=input(int("enter any number"))
+var2=input(int("enter any number"))
+
+print(var1+var2)
+
+
+
