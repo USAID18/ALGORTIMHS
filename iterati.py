a79c5e568447e9115dbf89d04f4d791e56e5a9a3
USAID198 <imranjamilke@gmail.com>
2024-12-27 19:55:16 +0500

    message

---
+++ iterati.py
@@ -0,0 +1,69 @@
+def printnNum(n):
+    iteration=0
+
+
+    print("NUMBER ENTER BY USER",n)
+
+    iteration+=1
+
+    print("total iteration by user",iteration,"/n")
+
+printnNum(20)
+    
+printnNum(30)
+    
+printnNum(50)
+
+print("/n our code wont change the time")
+
+
+
+# second
+def ountime(n): 
+    iteration=0
+    for i in range(1,n+1):
+
+     iteration+=1
+
+
+    print("when n is ,",n,"iteration=",iteration)
+
+
+
+ountime(20)
+
+ountime(40)
+
+ountime(60)
+
+print("/n in every,n, time increase")
+
+    # 3rd
+
+
+
+def ontimei(n):
+   iteration=0
+
+     
+   for i in range (0,n):
+     for j in range (0,n):
+      print("*",end="")
+
+      iteration+=1
+     print("")
+
+
+   print("/n when n is ",n,"iteration=",iteration,"/n")
+
+
+ontimei(3)
+ontimei(4)
+ontimei(6)
+
+
+
+
+
+  
+
